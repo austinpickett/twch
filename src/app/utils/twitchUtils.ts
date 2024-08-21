5d49415735b454cd2c00044be94db1a545ae5398
Austin Pickett <pickett.austin@gmail.com>
2024-08-21 15:18:47 -0400

    feat: next channel

--- src/app/utils/twitchUtils.ts
+++ src/app/utils/twitchUtils.ts
@@ -2,9 +2,7 @@ import axios from "axios";
 
 export async function getChannels(): Promise<string[]> {
   try {
-    const response = await axios.get(
-      "https://your-domain.com/api/getTwitchChannels"
-    );
+    const response = await axios.get("https://twch.xyz/api/getTwitchChannels");
     return response.data;
   } catch (error) {
     console.error("Error fetching channels:", error);
